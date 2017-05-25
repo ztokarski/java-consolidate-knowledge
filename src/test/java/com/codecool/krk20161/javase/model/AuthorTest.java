1363b19e7339e75b7ae99790fee3946283cb812f
Przemysław <przemyslaw.ciacka@gmail.com>
2017-05-25 23:38:01 +0200

    Add tests

---
+++ src/test/java/com/codecool/krk20161/javase/model/AuthorTest.java
@@ -0,0 +1,53 @@
+package com.codecool.krk20161.javase.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class AuthorTest {
+
+    @Test
+    @DisplayName("Name is set by constructor")
+    void testConstructorSetsNameInstanceVariable() {
+        String name = "Sourabh Sharma";
+        Author author = new Author(name);
+
+        assertEquals(name, author.getName());
+    }
+
+    @Test
+    @DisplayName("Biography is set by constructor")
+    void testConstructorSetsBiographyInstanceVariable() {
+        String biography = "Sourabh Sharma has been developing applications for over 10 years. " +
+                "He is a valued expert in creating, implementing, and testing multi-layered web applications.";
+        Author author = new Author(null, biography);
+
+        assertEquals(biography, author.getBiography());
+    }
+
+    @Nested
+    @DisplayName("when already instantiated")
+    class WhenAlreadyInstantiated {
+        private Author author;
+
+        @BeforeEach
+        void setUp() {
+            String name = "Sourabh Sharma";
+            String biography = "Sourabh Sharma has been developing applications for over 10 years. " +
+                    "He is a valued expert in creating, implementing, and testing multi-layered web applications.";
+            this.author = new Author(name, biography);
+        }
+
+        @Test
+        @DisplayName("Biography can be modified by setter")
+        void testBiographySetter() {
+            String newBiography = "This is new, very short biography...";
+            this.author.setBiography(newBiography);
+
+            assertEquals(newBiography, this.author.getBiography());
+        }
+
+    }
+
+}
---
+++ src/test/java/com/codecool/krk20161/javase/model/ReaderTest.java
@@ -0,0 +1,104 @@
+package com.codecool.krk20161.javase.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+class ReaderTest {
+
+    @Test
+    @DisplayName("Name is set by constructor")
+    void testNameIsSetByConstructor() {
+        String name = "John Kovalski";
+        Reader reader = new Reader(name);
+
+        assertEquals(name, reader.getName());
+    }
+
+    @Test
+    @DisplayName("Read list of book is empty by default")
+    void testReadListOfBookByDefault() {
+        Reader reader = new Reader(null);
+
+        assertEquals(0, reader.getRead().size());
+    }
+
+    @Test
+    @DisplayName("Read list of book is set by constructor")
+    void testReadIsSetByConstructor() {
+        List<Book> read = new ArrayList<Book>();
+        Reader reader = new Reader(null, read);
+
+        assertSame(read, reader.getRead());
+    }
+
+    @Nested
+    @DisplayName("with some read books")
+    class WithReadBooks {
+        Author author1 = new Author("Norman Chidley");
+        Author author2 = new Author("Dex Choudhury");
+        Author author3 = new Author("Corene Gian");
+
+        Book book1 = new Book("Foreigner Of Twilight", author1);
+        Book book2 = new Book("Mouse Of Greatness", author2);
+        Book book3 = new Book("Mice Of The Lost Ones", author3);
+        Book book4 = new Book("Aliens Without Flaws", author2);
+        Book book5 = new Book("Kings And Creators", author1);
+
+        Reader reader;
+
+        @BeforeEach
+        void setUp() {
+            List<Book> read = new ArrayList<Book>();
+            read.add(book1);
+            read.add(book2);
+            read.add(book3);
+            read.add(book4);
+            read.add(book5);
+
+            this.reader = new Reader(null, read);
+        }
+
+        @Test
+        @DisplayName("Book is found by title")
+        void testSearchBookByTitle() {
+            List<Book> expected = Arrays.asList(this.book3);
+
+            assertEquals(expected, this.reader.searchByTitle("Mice Of The Lost Ones"));
+        }
+
+        @Test
+        @DisplayName("Book is not found by title")
+        void testSearchBookByTitleThatDoesnNotExists() {
+            assertEquals(
+                    new ArrayList<Book>(),
+                    this.reader.searchByTitle("Some title that does not exists on read list"))
+            ;
+        }
+
+        @Test
+        @DisplayName("Book is found by author")
+        void testSearchByAuthor() {
+            List<Book> expected = Arrays.asList(this.book2, this.book4);
+            Author author = new Author("Dex Choudhury");
+
+            assertEquals(expected, this.reader.searchByAuthor(author));
+        }
+
+        @Test
+        @DisplayName("Book is not found by author")
+        void testSearchBookByAuthorThatDoesnNotExists() {
+            Author author = new Author("Unknown");
+
+            assertEquals(new ArrayList<Book>(), this.reader.searchByAuthor(author));
+        }
+    }
+
+}
