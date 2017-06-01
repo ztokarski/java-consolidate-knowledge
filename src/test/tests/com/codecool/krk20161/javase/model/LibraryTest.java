3bbc804bc751043e90a72b50115ad8d08efd4477
zBIGniew <zbigniew.piotr.tokarski@gmail.com>
2017-06-02 01:39:23 +0200

    Run all Library test

--- src/test/tests/com/codecool/krk20161/javase/model/LibraryTest.java
+++ src/test/tests/com/codecool/krk20161/javase/model/LibraryTest.java
@@ -1,30 +1,30 @@
-//package com.codecool.krk20161.javase.model;
-//
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//
-//import java.util.ArrayList;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class LibraryTest {
-//
-//    @Test
-//    @DisplayName("Catalog is empty by default")
-//    void testCatalogIsEmptyByDefault() {
-//        Library library = new Library();
-//
-//        assertEquals(new ArrayList<Book>(), library.getCatalog().getBooks());
-//    }
-//
-//    @Test
-//    @DisplayName("Catalog is set by setter")
-//    void testCatalogIsSetBySetter() {
-//        Catalog catalog = new Catalog();
-//        Library library = new Library();
-//        library.setCatalog(catalog);
-//
-//        assertSame(catalog, library.getCatalog());
-//    }
-//
-//}
+package com.codecool.krk20161.javase.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LibraryTest {
+
+    @Test
+    @DisplayName("Catalog is empty by default")
+    void testCatalogIsEmptyByDefault() {
+        Library library = new Library();
+
+        assertEquals(new ArrayList<Book>(), library.getCatalog().getBooks());
+    }
+
+    @Test
+    @DisplayName("Catalog is set by setter")
+    void testCatalogIsSetBySetter() {
+        Catalog catalog = new Catalog();
+        Library library = new Library();
+        library.setCatalog(catalog);
+
+        assertSame(catalog, library.getCatalog());
+    }
+
+}
