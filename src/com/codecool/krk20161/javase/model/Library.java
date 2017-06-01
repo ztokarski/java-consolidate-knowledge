06c2c5707c19795c5ef0a35cc686f4a219b380c2
zBIGniew <zbigniew.piotr.tokarski@gmail.com>
2017-06-02 01:38:56 +0200

    Add Library class

--- src/com/codecool/krk20161/javase/model/Library.java
+++ src/com/codecool/krk20161/javase/model/Library.java
@@ -1,5 +1,20 @@
 package com.codecool.krk20161.javase.model;
 
+import java.util.ArrayList;
+
 public class Library {
+    private Catalog catalog;
+
+    public Library() {
+        this.catalog = new Catalog();
+    }
+
+    public Catalog getCatalog() {
+        return catalog;
+    }
+
+    public void setCatalog(Catalog catalog) {
+        this.catalog = catalog;
+    }
 
 }
